3aa9a865d52cc5175f853d24282e183dea6012ad
b62303411 <samuel.ars@gmail.com>
2023-01-02 21:35:19 -0500

    tabulation removed for space, added precision on gravity constant.

--- gwave_math.hlsl
+++ gwave_math.hlsl
@@ -5,135 +5,135 @@
 
 float CalculateWaveNumber(float wavelength)
 {
-	float g = 9.81;
-	float pi = 3.14159265358979323846;
-	return 2 * pi / wavelength;
+    float g = 9.80665;
+    float pi = 3.14159265358979323846;
+    return 2 * pi / wavelength;
 }
 
 float CalculateFrequency(float wavelength, float steepness)
 {
-	float g = 9.81;
-	float pi = 3.14159265358979323846;
-	float k = CalculateWaveNumber(wavelength);
-	float omega = sqrt(g * k);
-	return omega / (2 * pi);
+    float g = 9.80665;
+    float pi = 3.14159265358979323846;
+    float k = CalculateWaveNumber(wavelength);
+    float omega = sqrt(g * k);
+    return omega / (2 * pi);
 }
 
 float CalculatePeriod(float frequency)
 {
-	return 1 / frequency;
+    return 1 / frequency;
 }
 
 // Calculate the amplitude of a wave given its steepness and wavelength
 float CalculateAmplitude(float steepness, float wavelength)
 {
-	float g = 9.81;
-	float pi = 3.14159265358979323846;
+    float g = 9.80665;
+    float pi = 3.14159265358979323846;
 
-	// Calculate the wavenumber of the wave
-	float wavenumber = 2 * pi / wavelength;
+    // Calculate the wavenumber of the wave
+    float wavenumber = 2 * pi / wavelength;
 
-	// Calculate the amplitude of the wave using the formula provided above
-	float amplitude = steepness / wavenumber;
+    // Calculate the amplitude of the wave using the formula provided above
+    float amplitude = steepness / wavenumber;
 
-	// Return the calculated amplitude
-	return amplitude;
+    // Return the calculated amplitude
+    return amplitude;
 }
 
 float CalculateAngularFrequency(float period)
 {
-	float g = 9.81;
-	float pi = 3.14159265358979323846;
-	return 2 * pi / period;
+    float g = 9.80665;
+    float pi = 3.14159265358979323846;
+    return 2 * pi / period;
 }
 float2 uv;
 ;
 void GerstnerWave(float3 position, float3 direction, float wavelength, float steepness, inout float3 displacement, inout float3 tangent, inout float3 binormal)
 {
-	float g = 9.81;
-	float pi = 3.14159265358979323846;	
-	// Parameters for the wave
+    float g = 9.80665;
+    float pi = 3.14159265358979323846;
+    // Parameters for the wave
 
-	// Compute the wave frequency and speed from the steepness and wavelength
-	float frequency = g / (2 * pi * wavelength);
-	
-	float angularFrequency = sqrt(g * wavelength);
-	
-	//float waveNumber = CalculateWaveNumber(wavelength);
-	float wavenumber = 2 * pi / wavelength;
+    // Compute the wave frequency and speed from the steepness and wavelength
+    float frequency = g / (2 * pi * wavelength);
 
-	//km = sqrt(pow(k.x,2)+ pow(k.z,2));
-	float3 k = direction * wavenumber;
+    float angularFrequency = sqrt(g * wavelength);
 
-	float w = sqrt(g * wavenumber);
+    //float waveNumber = CalculateWaveNumber(wavelength);
+    float wavenumber = 2 * pi / wavelength;
 
-	float theta = dot(k.xz, position.xz) - (w * _Time.y);
+    //km = sqrt(pow(k.x,2)+ pow(k.z,2));
+    float3 k = direction * wavenumber;
 
-	float amplitude = CalculateAmplitude(steepness, wavelength);
-	
-	float km = wavenumber;
+    float w = sqrt(g * wavenumber);
 
-	displacement.x +=  - direction.x * amplitude * sin(theta);
-	displacement.y += amplitude * cos(theta);
-	displacement.z +=  - direction.z * amplitude * sin(theta);
-	
+    float theta = dot(k.xz, position.xz) - (w * _Time.y);
+
+    float amplitude = CalculateAmplitude(steepness, wavelength);
+
+    float km = wavenumber;
+
+    displacement.x += -direction.x * amplitude * sin(theta);
+    displacement.y += amplitude * cos(theta);
+    displacement.z += -direction.z * amplitude * sin(theta);
+
     // ds/dx partial derivative for the tengeant.
-    tangent.x += - (amplitude* k.x * k.x * cos(theta))/km;
-	tangent.y += -amplitude * k.x * sin(theta);
-	tangent.z += -(amplitude * k.x * k.z * cos(theta))/km;
-	
-	// ds/dz partial derivative for the binormal.
-	binormal.x += - ((amplitude * k.x * k.z * cos(theta))/km);
-	binormal.y += - (amplitude * k.z * sin(theta));
-	binormal.z += - ((amplitude * k.z * k.z * cos(theta))/km);
+    tangent.x += -(amplitude * k.x * k.x * cos(theta)) / km;
+    tangent.y += -amplitude * k.x * sin(theta);
+    tangent.z += -(amplitude * k.x * k.z * cos(theta)) / km;
 
-	// Return the displacement as a float3 value
-	
+    // ds/dz partial derivative for the binormal.
+    binormal.x += -((amplitude * k.x * k.z * cos(theta)) / km);
+    binormal.y += -(amplitude * k.z * sin(theta));
+    binormal.z += -((amplitude * k.z * k.z * cos(theta)) / km);
+
+    // Return the displacement as a float3 value
+
 }
 
 
-void gw(float3 position,float4 wave, inout float3 displacement,inout float3 tangent, inout float3 binormal)
+void gw(float3 position, float4 wave, inout float3 displacement, inout float3 tangent, inout float3 binormal)
 {
 
-	float wl = wave.x;
-	float s = wave.y;
-	float3 direction;
-	
-	direction.x = wave.z;
-	direction.y = 0;
-	direction.z = wave.w;
-	
-	float3 normalized_direction = normalize(direction);
+    float wl = wave.x;
+    float s = wave.y;
+    float3 direction;
 
-	GerstnerWave(position, normalized_direction, wl, s, displacement, tangent, binormal);
+    direction.x = wave.z;
+    direction.y = 0;
+    direction.z = wave.w;
+
+    float3 normalized_direction = normalize(direction);
+
+    GerstnerWave(position, normalized_direction, wl, s, displacement, tangent, binormal);
 }
 
-void  gwaves_math_float(float3 position,float4 w1, float4 w2, float4 w3, float4 w4,float4 w5,  out float3 displacement, out float3 normal, out float3 tangent)
+void  gwaves_math_float(float3 position, float4 w1, float4 w2, float4 w3, float4 w4, float4 w5, out float3 displacement, out float3 normal, out float3 tangent)
 {
-	tangent.x = 1;
-	tangent.y = 0;
-	tangent.z = 0;
+    tangent.x = 1;
+    tangent.y = 0;
+    tangent.z = 0;
 
-	float3 binormal;
-	binormal.x = 0;
-	binormal.y = 0;
-	binormal.z = 1;
-	displacement.x=position.x;
-	displacement.y=0;
-	displacement.z=position.z;
-	gw(position, w1, displacement, tangent, binormal);
-	gw(position, w2, displacement, tangent, binormal);
-	gw(position, w3, displacement, tangent, binormal);
-	gw(position, w4, displacement, tangent, binormal);
-	gw(position, w5, displacement, tangent, binormal);
-	normal  = normalize(cross(tangent, binormal));
-	tangent = normalize(tangent);
+    float3 binormal;
+    binormal.x = 0;
+    binormal.y = 0;
+    binormal.z = 1;
+    displacement.x = position.x;
+    displacement.y = 0;
+    displacement.z = position.z;
+    gw(position, w1, displacement, tangent, binormal);
+    gw(position, w2, displacement, tangent, binormal);
+    gw(position, w3, displacement, tangent, binormal);
+    gw(position, w4, displacement, tangent, binormal);
+    gw(position, w5, displacement, tangent, binormal);
+    normal = normalize(cross(tangent, binormal));
+    tangent = normalize(tangent);
 
-	// Generate the gradient texture
-	//float color = lerp(float4(0, 0, 0, 1), float4(1, 1, 1, 1), uv.x);
+    // Generate the gradient texture
+    //float color = lerp(float4(0, 0, 0, 1), float4(1, 1, 1, 1), uv.x);
 
-	// Write the gradient texture to the output texture
-	//tex2Dproj(tex, float4(uv, 0, 1)) = color;
+    // Write the gradient texture to the output texture
+    //tex2Dproj(tex, float4(uv, 0, 1)) = color;
 
 
 }
